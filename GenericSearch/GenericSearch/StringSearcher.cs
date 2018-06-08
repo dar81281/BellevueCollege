46d526fcbf44f120eb277898b6c52d280fa0a391
Cory Nunn <corynunn@yahoo.com>
2018-06-07 22:00:14 -0700

    Just realized that the search I wrote was an OR search, so I also supplied an AND search. Maybe we can put that in the GUI?

---
+++ GenericSearch/GenericSearch/StringSearcher.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericSearch
+{
+    public static class StringSearcher
+    {
+        /// <summary>
+        /// Takes two arrays of strings and searches the first for the queries from the second. Will return true if any of the searched for elements are found.
+        /// </summary>
+        /// <param name="dataSource"></param>
+        /// <param name="queries"></param>
+        /// <returns></returns>
+        public static bool StringArrayOrSearch(string[] dataSource, string[] queries)
+        {
+            foreach (string str in dataSource)
+            {
+                foreach (string q in queries)
+                {
+                    if (str.ToLower().Contains(q.ToLower().Trim()))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// If I wrote this correctly it should return true only if each query is found in the datasource.
+        /// </summary>
+        /// <param name="dataSource"></param>
+        /// <param name="queries"></param>
+        /// <returns></returns>
+        public static bool StringArrayAndSearch(string[] dataSource, string[] queries)
+        {
+            foreach (string str in dataSource)
+            {
+                bool queryMatch = false;
+                foreach (string q in queries)
+                {
+                    if (str.ToLower().Contains(q.ToLower().Trim()))
+                    {
+                        queryMatch = true;
+                    }
+                }
+                if (queryMatch == false)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
