e9600a8ac410f3c0269f0f335f48449c8d962828
rasler.douglas <rasler.douglas@V2102-08>
2018-06-14 18:16:29 -0700

    merging stuff from master into doug branch

---
+++ LeftoversRecipeApp/RecipeClassLibrary/Dessert.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeClassLibrary
+{
+    class Dessert : Recipe
+    {
+        public string type { get; set; }
+
+        public override string ToString()
+        {
+            return type + "-" + base.Title;
+        }
+    }
+}
---
+++ LeftoversRecipeApp/RecipeClassLibrary/MealItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeClassLibrary
+{
+    class MealItem : Recipe
+    {
+        public string type { get; set; }
+
+        public override string ToString()
+        {
+            return type + "-" + base.Title;
+        }
+    }
+}
